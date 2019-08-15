4f97db70bf61ed564e7f589ebcaeafc8f6ed4010
neaj mahmud <neajmahmud230@gmail.com>
2019-08-15 12:27:31 -0400

    first commit

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Switch, Route} from "react-router-dom"
+import Home from "../src/components/Home"
+
+
+export default (    
+    <Switch>
+       
+        <Route path="/" component={Home}/>
+
+        
+
+
+    </Switch>
+)
