4dd4bb6fc8fa469d73a367a831eccc973f67814e
neaj mahmud <neajmahmud230@gmail.com>
2019-08-19 14:51:35 -0400

    working with the slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react'
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import Items from "./Items"
 
 export default class Home extends Component {
     render() {
         return (
-            <div style={{margin:"20px auto", height:"60vh", width:"60vw", border:"1px red solid"}}>
-        <CarouselProvider
-                naturalSlideWidth={10}
-                naturalSlideHeight={10}
-                totalSlides={3}
-                isPlaying="true"
-                interval={3000}
-        >
-            <Slider style={{height:"70vh", width:"60vw"}}>
-                <Slide index={0}><img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/67653225_1100441646821967_956068551785847480_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&se=7&oh=d0dbbafeada04c9d1320cd1a27b5e92a&oe=5DDB7154&ig_cache_key=MjA5NzY1MDY4Nzk0ODI0NDU4MA%3D%3D.2"></img></Slide>
-                <Slide index={1}><img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/67653225_1100441646821967_956068551785847480_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&se=7&oh=d0dbbafeada04c9d1320cd1a27b5e92a&oe=5DDB7154&ig_cache_key=MjA5NzY1MDY4Nzk0ODI0NDU4MA%3D%3D.2"></img></Slide>
-                <Slide index={2}><img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/67653225_1100441646821967_956068551785847480_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&se=7&oh=d0dbbafeada04c9d1320cd1a27b5e92a&oe=5DDB7154&ig_cache_key=MjA5NzY1MDY4Nzk0ODI0NDU4MA%3D%3D.2"></img></Slide>
-            </Slider>
-            
-      </CarouselProvider>
+            <div>
+                <div style={{height:"100"}}>
+
+                    <Carousel
+                    autoPlay="true"
+                    infiniteLoop="true"
+                    interval="2000"
+                    width="80vw"
+                    >
+                    <div>
+                        <img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/67653225_1100441646821967_956068551785847480_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&se=7&oh=d0dbbafeada04c9d1320cd1a27b5e92a&oe=5DDB7154&ig_cache_key=MjA5NzY1MDY4Nzk0ODI0NDU4MA%3D%3D.2" />
+                        <p className="legend">Legend 1</p>
+                    </div>
+                    <div>
+                        <img src="https://http2.mlstatic.com/powerus-pw2500-1-ohm-D_NQ_NP_761466-MLM28552680388_112018-F.jpg" />
+                        <p className="legend">Legend 2</p>
+                    </div>
+                    <div>
+                        <img src="https://scontent-lht6-1.cdninstagram.com/v/t51.2885-15/e35/67653225_1100441646821967_956068551785847480_n.jpg?_nc_ht=scontent-lht6-1.cdninstagram.com&se=7&oh=d0dbbafeada04c9d1320cd1a27b5e92a&oe=5DDB7154&ig_cache_key=MjA5NzY1MDY4Nzk0ODI0NDU4MA%3D%3D.2" />
+                        <p className="legend">Legend 3</p>
+                    </div>
+                    </Carousel>
+                </div>
+                   
+                    <div>
+                        <Items/>
+                    </div>
             </div>
         )
     }
